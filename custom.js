7862c1263a516598a688ac4c1800bc8ee733acb8
Corey Decker <coreymdecker@gmail.com>
2017-02-16 14:17:55 -0500

    added Support tab

--- custom.js
+++ custom.js
@@ -1,5 +1,5 @@
 $(document).ready(function(){
- 
+
 	var box = $('#container2 span:contains("No assigned training")');
  if(box.length > 0){
    box.parents('#container2').children(':contains("No assigned training")').hide();
@@ -10,5 +10,7 @@ $(document).ready(function(){
     $( ".smenu.sm.navTabs" ).append(supporttab);
 }
  
+  	$smjquery('#main-menu').smartmenus('refresh')
+  	
 	});
   
