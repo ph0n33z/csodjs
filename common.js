4fe9ae23d7292d555f46b74a960db620ded6c740
Corey Decker <coreymdecker@gmail.com>
2017-02-16 12:19:44 -0500

    trying this again

--- common.js
+++ common.js
@@ -1,4 +1,4 @@
-$(function() {
+$(document).ready(function(){
 
 $smjquery('#main-menu').smartmenus('refresh')
 
@@ -17,6 +17,8 @@ var box = $('#container2 span:contains("No assigned training")');
  if(box.length > 0){
    box.parents('#container2').children(':contains("No assigned training")').hide();
 }
+
+$("body").html("jQuery is working");
 
 });
 
