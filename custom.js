f05689050fb2a4ad3d229d17fd2177efc23cf0bf
Corey Decker <coreymdecker@gmail.com>
2017-02-16 15:03:57 -0500

    added remaining scripts from udev

--- custom.js
+++ custom.js
@@ -9,4 +9,16 @@ $(document).ready(function(){
    	box.parents('#container2').children(':contains("No assigned training")').hide();
 }
 
+	$('.cso-txt span:contains("Online Class")').each(function(){
+ 	$(this).html($(this).html().replace('Online Class','Web-Based Training'));
+	});
+
+	$('[data-original-title="Online Class"]').attr('data-original-title', 'changed tooltip');
+     $('.tooltip-cso').tooltip();
+
+	$('.cso-li-filter li:contains("Popular")').hide();
+
+	$('.item.oc').css({'background','url("https://placeholdit.imgix.net/~text?txtsize=33&txt=61×61&w=61&h=61")'});
+
+
 	});
