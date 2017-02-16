d49b89189e9ef7eee790f7ca150d05fa8b20eb13
Corey Decker <coreymdecker@gmail.com>
2017-02-16 11:46:27 -0500

    removed docread

--- common.js
+++ common.js
@@ -1,5 +1,4 @@
-$(document).ready(function()    {
-        
+     
 $smjquery('#main-menu').smartmenus('refresh')
 
 $('.cso-txt span:contains("Online Class")').each(function(){
@@ -17,5 +16,3 @@ var box = $('#container2 span:contains("No assigned training")');
  if(box.length > 0){
    box.parents('#container2').children(':contains("No assigned training")').hide();
 }
-
-});
